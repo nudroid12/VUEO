0303b6a6e41e9670ae6fe25f1d8e1f47da3d03d3
nudroid12 <nudroidlabs@gmail.com>
2026-08-29 00:59:55 +0800

    Add files via upload

--- app/src/main/java/com/vueo/app/ui/VueoApp.kt
+++ app/src/main/java/com/vueo/app/ui/VueoApp.kt
@@ -42,6 +42,7 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items as gridItems
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
